bd4c8123fd4ff0a099aaa5b66477e5e103829d00
Eugene2702 <zhenyagodin@mail.ru>
2022-11-04 21:37:24 +0300

    reg

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 app = Flask(__name__)
 
@@ -14,13 +13,52 @@ cursor = conn.cursor()
 def index():
     return render_template('login.html')
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
 
-    return render_template('account.html', full_name=records[0][1])
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            if len("".join(username.split())) == 0 or len("".join(password.split())) == 0:
+                error = 'Неверно заполненые поля'
+                return render_template('login.html', error=error)
+
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+
+            if len(records) == 0:
+                errorLogin = 'Неверно введеные данные'
+                return render_template('login.html', errorLogin=errorLogin)
+
+
+            return render_template('account.html', full_name=records[0][1], username=records[0][2],
+                                   password=records[0][3])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        if len("".join(login.split())) == 0 or len("".join(password.split())) == 0 or len("".join(name.split())):
+            errorReg = 'Неверно заполненые поля'
+            return render_template('registration.html', errorReg=errorReg)
+
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+
+    return render_template('registration.html')
+
+
+
+
 
 
